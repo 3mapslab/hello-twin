d0a92fe08025a617b41bd6d35e8804ca4f1c9d9e
Rui Jorge Ramos <rui.j.ramos@inesctec.pt>
2021-01-27 11:14:39 +0000

    trying to define geojson vt

--- src/digitaltwin/TwinView.js
+++ src/digitaltwin/TwinView.js
@@ -3,6 +3,7 @@ import CameraControls from "camera-controls";
 import { MapView } from "./GeoThree/MapView";
 import { MapBoxProvider } from "./GeoThree/providers/MapBoxProvider";
 import { UnitsUtils } from "./GeoThree/utils/UnitsUtils";
+import geojsonvt from 'geojson-vt';
 
 const key = "pk.eyJ1IjoidHJpZWRldGkiLCJhIjoiY2oxM2ZleXFmMDEwNDMzcHBoMWVnc2U4biJ9.jjqefEGgzHcutB1sr0YoGw";
 
@@ -79,6 +80,39 @@ export default class TwinView {
         const delta = this.clock.getDelta();
         this.controls.update(delta);
         this.renderer.render(this.scene, this.camera);
+    }
+
+    geojsonVT() {
+
+        fetch("bollards.geo.json")
+            .then((res) => res.json())
+            .then((out) => {
+                // build an initial index of tiles
+                var tileIndex = geojsonvt(out, {
+                    maxZoom: 24,  // max zoom to preserve detail on; can't be higher than 24
+                    tolerance: 3, // simplification tolerance (higher means simpler)
+                    extent: 4096, // tile extent (both width and height)
+                    buffer: 64,   // tile buffer on each side
+                    debug: 0,     // logging level (0 to disable, 1 or 2)
+                    lineMetrics: false, // whether to enable line metrics tracking for LineString/MultiLineString features
+                    promoteId: null,    // name of a feature property to promote to feature.id. Cannot be used with `generateId`
+                    generateId: false,  // whether to generate feature ids. Cannot be used with `promoteId`
+                    indexMaxZoom: 20,       // max zoom in the initial tile index
+                    indexMaxPoints: 100 // max number of points per tile in the index
+                });
+                console.log(tileIndex);
+
+                // request a particular tile
+                //var features = tileIndex.getTile(3, 0, 1).features;
+                //console.log(features);
+
+                // show an array of tile coordinates created so far
+                console.log(tileIndex.tileCoords); // [{z: 0, x: 0, y: 0}, ...]
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
     }
 
 }
