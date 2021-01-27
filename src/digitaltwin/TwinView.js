e82dc96289b81cc5fa989af7a5a4b4b4c07192c7
João Pedro Silva <joaopblosilva@hotmail.com>
2021-01-27 17:08:45 +0000

    Implemented mergeBufferGeometries with multiple colors [refs #2]

--- src/digitaltwin/TwinView.js
+++ src/digitaltwin/TwinView.js
@@ -3,6 +3,7 @@ import CameraControls from "camera-controls";
 import { MapView } from "./GeoThree/MapView";
 import { MapBoxProvider } from "./GeoThree/providers/MapBoxProvider";
 import { UnitsUtils } from "./GeoThree/utils/UnitsUtils";
+import TwinMesh from "./TwinMesh";
 
 const key = "pk.eyJ1IjoidHJpZWRldGkiLCJhIjoiY2oxM2ZleXFmMDEwNDMzcHBoMWVnc2U4biJ9.jjqefEGgzHcutB1sr0YoGw";
 
@@ -19,34 +20,26 @@ export default class TwinView {
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0x96CDCD);
 
-        //Creation of Camera
-        this.camera = new THREE.PerspectiveCamera(
-            60,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            10000
-        );
-        this.camera.position.set(0, 350, 0);
+        //Init Camera
+        this.camera = null;
+        this.initCamera();
 
-        //Creation of Renderer
-        this.renderer = new THREE.WebGLRenderer({
-            canvas, antialias: true,
-            powerPreference: "high-performance",
-            physicallyCorrectLights: true
-        });
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.setPixelRatio(window.devicePixelRatio);
+        //Init Renderer
+        this.renderer = null;
+        this.initRenderer();
 
-        //Lights
-        this.light = new THREE.AmbientLight(0xffffff, 0.65);
-        this.scene.add(this.light);
+        //Init Lights
+        this.light = null;
+        this.initLights();
 
-        //Camera Controls
-        this.controls = new CameraControls(this.camera, this.renderer.domElement);
-        this.controls.verticalDragToForward = true;
-        this.controls.dollyToCursor = false;
+        //Init Camera Controls
+        this.controls = null;
+        this.initCameraControls();
         this.clock = new THREE.Clock();
 
+        // Puxar o mapa da posição original para o centro do mundo (0,0,0)
+        this.coords = UnitsUtils.datumsToSpherical(this.configs.initialPosition.lat, this.configs.initialPosition.lng);
+        //Init map
         this.map = null;
         this.initMap();
 
@@ -56,15 +49,55 @@ export default class TwinView {
         this.animate();
     }
 
+    initCamera() {
+        //Creation of Camera
+        this.camera = new THREE.PerspectiveCamera(
+            60,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            10000
+        );
+        this.camera.position.set(0, 350, 0);
+    }
+
+    initRenderer() {
+        //Creation of Renderer
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: this.canvas, antialias: true,
+            powerPreference: "high-performance",
+            physicallyCorrectLights: true
+        });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+    }
+
+    loadLayerToScene(layerCode, geojson, properties, point){
+        let twinMesh = new TwinMesh();
+        let mergedMeshes = twinMesh.loadLayer(layerCode, geojson, properties, point, this.coords);
+        this.scene.add(mergedMeshes);
+    }
+
+    initLights() {
+        //Lights
+        this.light = new THREE.AmbientLight(0xffffff, 0.65);
+        this.scene.add(this.light);
+    }
+
+    initCameraControls() {
+        //Camera Controls
+        this.controls = new CameraControls(this.camera, this.renderer.domElement);
+        this.controls.verticalDragToForward = true;
+        this.controls.dollyToCursor = false;
+    }
+
     initMap() {
         // Create a map tiles provider object
         var provider = new MapBoxProvider(key, "mapbox/streets-v10", MapBoxProvider.STYLE);
 
         // Create the map view and add it to your THREE scene
         this.map = new MapView(MapView.PLANAR, provider);
-        // Puxar o mapa da posição original para o centro do mundo (0,0,0)
-        var coords = UnitsUtils.datumsToSpherical(this.configs.initialPosition.lat, this.configs.initialPosition.lng);
-        this.map.position.set(-coords.x, 0, coords.y);
+
+        this.map.position.set(-this.coords.x, 0, this.coords.y);
         this.map.updateMatrixWorld(true);
         this.scene.add(this.map);
     }
